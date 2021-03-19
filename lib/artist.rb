8bb376bb3c2c2d267cca4388d47670fe25a18a3e
Kyle Samuel <Ksamuel92@gmail.com>
2021-03-19 08:48:40 -0400

    commit tree 3328bdba39d91b38bb8a5324183df23ab5f5655b

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,29 @@
+require "pry"
+class Artist
+    attr_accessor :name, :songs
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+        @songs = []
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_song(song_name, genre)
+        #binding.pry
+        Song.new(song_name,self,genre)
+    
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def genres
+        songs.collect {|artist| artist.genre} 
+    end
+end
